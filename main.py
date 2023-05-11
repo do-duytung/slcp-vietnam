fd619e3e62f7dacebfff29c0c5e068800a165df3
do-duytung <124649128+do-duytung@users.noreply.github.com>
2023-05-11 20:10:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,16 +24,15 @@ except KeyError:
   token = "Token not available"
 
 ids = ['A230398', 'A230383', 'A230626', 'A230443', 'A231934', 'A364435', 'A358570', 'A06487']
-def scraping():
-  for i in ids:
-    id = i
-    url = link + id +'/?token=' + token
-    response = requests.get(url)
-    data = json.loads(response.text)
-    time = data['data']['time']['s']
-    aqi = data['data']['aqi']
-    List = [time, id, aqi]
-    with open('purpleair.csv', 'a') as f:
-      writer = csv.writer(f)
-      writer.writerow(List)
-    print(List)
+
+for i in ids:
+  id = i
+  url = link + id +'/?token=' + token
+  response = requests.get(url)
+  data = json.loads(response.text)
+  time = data['data']['time']['s']
+  aqi = data['data']['aqi']
+  List = [time, id, aqi]
+  with open('purpleair.csv', 'a') as f:
+    writer = csv.writer(f)
+    writer.writerow(List)
